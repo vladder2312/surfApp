a18eabcd38c13f53e9006f562e1d6c575a41fd79
Vlad Styazhkin <vladder2312@mail.ru>
2019-08-19 16:23:41 +0300

    Сплеш с переходом на экран с авторизацией

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -1,2 +1,14 @@
 package com.example.myapplication
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        supportActionBar?.hide()
+        println("Активность создана")
+    }
+}
--- app/src/main/java/com/example/myapplication/Splash.kt
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -1,13 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
-class First : AppCompatActivity() {
+class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
-        getSupportActionBar()?.hide()
+        setVisible(true)
+        supportActionBar?.hide()
+        showNextActivity()
     }
+
+    private fun showNextActivity(){
+        val loginIntent = Intent(this, Login::class.java)
+        Timer().schedule(object : TimerTask(){
+                override fun run() { startActivity(loginIntent) }
+            }, 300 )
+    }
+
 }
