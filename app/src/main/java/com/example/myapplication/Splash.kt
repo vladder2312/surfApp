d7626d4413b168b68aca5e57838794f1756feae3
Vlad Styazhkin <vladder2312@mail.ru>
2019-08-20 17:19:44 +0300

    Дорисовать PrBar и Btn

--- app/src/main/java/com/example/myapplication/Splash.kt
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import java.util.*
 
 class Splash : AppCompatActivity() {
@@ -10,6 +11,7 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         showNextActivity()
     }
 
